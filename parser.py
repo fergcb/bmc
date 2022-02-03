0813468afaee5312da7201a25b86067dc92333a9
fergcb <fergus.bentley@gmail.com>
2022-02-03 18:22:30 +0000

    Use parser combinators

--- parser.py
+++ parser.py
@@ -1,6 +1,14 @@
 import re
 
-def parse_num(source):
+def WS(source):
+    re_match = re.match(r"^[ \t]+", source)
+    if re_match is not None:
+        match = re_match.group(0)
+        rest = source[len(match):]
+        return ({ "type": "nop", "value": "_" }, rest)
+    return (None, source)
+
+def Num(source):
     re_match = re.match(r"^(0|([1-9][0-9]{,2}))", source)
     if re_match is not None:
         match = re_match.group(0)
@@ -9,7 +17,7 @@ def parse_num(source):
     return (None, source)
 
 
-def parse_op(source):
+def Op(source):
     if source[0] in "+-*/%.":
         match = source[0]
         rest = source[1:]
@@ -17,7 +25,44 @@ def parse_op(source):
     return (None, source)
 
 
-parse_funcs = [parse_num, parse_op]
+def Sequence(*matchers):
+    def match_sequence(source):
+        matches = []
+        rest = source
+        for matcher in matchers:
+            match, rest = matcher(rest)
+            if match is None:
+                return (None, source)
+            matches.append(match)
+        return (matches, rest)
+    return match_sequence
+        
+
+def Option(*matchers):
+    def match_option(source):
+        for matcher in matchers:
+            match, rest = matcher(source)
+            if match is None:
+                continue
+            return (match, rest)
+        return (None, source)
+    return match_option
+        
+
+def ZeroOrMore(matcher):
+    def match_zero_or_more(source):
+        matches = []
+        match, rest = {}, source
+        while len(rest) > 0:
+            match, rest = matcher(rest)
+            if match is None:
+                return (None, rest)
+            matches.append(match)
+        return (matches, rest)
+    return match_zero_or_more
+
+
+parser = ZeroOrMore(Option(WS, Op, Num))
 
 
 def strip_whitespace(source):
@@ -25,16 +70,14 @@ def strip_whitespace(source):
 
 
 def tokenize(source):
-    tokens = []
-    rest = source
-    while len(rest) > 0:
-        rest = rest.lstrip()
-        for parse in parse_funcs:
-            match, rest = parse(rest)
-            if match is not None:
-                tokens.append(match)
-                break
-        else:
-            col = len(source) - len(rest)
-            raise Exception(f"No match found at col {col}.")
+    tokens, rest = parser(source)
+
+    if tokens is None:
+        col = len(source) - len(rest)
+        raise Exception(f"No match found at col {col}.")
+
+    if rest != "":
+        col = len(source) - len(rest)
+        raise Exception(f"Expected end of string, found '{rest[:1]}' at col {col}.")
+
     return tokens
